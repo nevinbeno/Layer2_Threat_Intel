67d8743929d87618b8dcdb8e4054ff7308801f20
vishakhayelmar-lab <vishakhayelmar@gmail.com>
2025-12-11 23:33:57 +0530

    Refactor VirusTotal test script to use placeholder API key

--- Vishakha/virustotal_test.py
+++ Vishakha/virustotal_test.py
@@ -1,6 +1,6 @@
 import requests
 
-api_key = "3ed80728e4d4cde21539d8e34c0556dca327f56180980c4276f8b0a36e2f4fdb"
+api_key = "key"
 hash_value = "44d88612fea8a8f36de82e1278abb02f"  # test hash
 
 url = f"https://www.virustotal.com/api/v3/files/{hash_value}"
@@ -31,3 +31,4 @@ print("Malicious:", stats.get("malicious"))
 print("Suspicious:", stats.get("suspicious"))
 print("Undetected:", stats.get("undetected"))
 print("Harmless:", stats.get("harmless"))
+
